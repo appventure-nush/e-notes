abdf5c9394a1746c551d9fc31bd97c3de46dc733
octo-kumo <h1710169@nushigh.edu.sg>
2021-01-31 20:28:54 +0900

    bug fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,15 @@
 window.GEBI = id => document.getElementById(id);
 window.qs = tag => document.querySelector(tag);
 window.qsa = tag => document.querySelectorAll(tag);
+const listenerCache = {};
+window.addChangeListener = function (el, ov, gt = el => el.value, tg = el) {
+    if (typeof gt !== 'function') [tg, gt] = [gt, tg];
+    if (listenerCache[el.id]) el.removeEventListener('change', listenerCache[el.id]);
+    if (tg.classList.contains('changed')) tg.classList.remove('changed');
+    el.addEventListener('change', listenerCache[el.id] = () => {
+        if (!tg.classList.contains('changed') && ov !== gt(el)) tg.classList.add('changed');
+        else if (tg.classList.contains('changed') && ov === gt(el)) tg.classList.remove('changed');
+    });
+}
+
 // the above code is just to make code shorter
