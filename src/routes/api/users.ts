f123785078f59e8deb45609b01c3aa4d5f834624
octo-kumo <h1710169@nushigh.edu.sg>
2021-10-30 14:10:43 +0800

    delete user btn (old change)

--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -26,7 +26,28 @@ users.post("/:uid/admin", checkAdmin, async (req, res) => {
         const user = await getUser(req.params.uid);
         if (!user) return res.status(404).json({
             reason: "user_not_found",
-            rid: req.params.uid
+            uid: req.params.uid
+        });
+        if (Array.isArray(req.body.roles)) user.roles = Array.from(req.body.roles).map(el => String(el));
+        if (typeof req.body.admin === 'boolean') user.admin = req.body.admin;
+        if (typeof req.body.permissions === 'object') _setPermissions(user, req.body.permissions);
+        await updateUser(user.uid, user);
+        res.json(user);
+    } catch (e) {
+        res.status(500).send("failed")
+    }
+});
+
+users.delete("/:uid", checkAdmin, async (req, res) => {
+    try {
+        const user = await getUser(req.params.uid);
+        if (!user) return res.status(404).json({
+            reason: "user_not_found",
+            uid: req.params.uid
+        });
+        return res.status(404).json({
+            reason: "test",
+            uid: req.params.uid
         });
         if (Array.isArray(req.body.roles)) {
             await addAudit(simpleAudit(req.body.cuid, req.params.rid, Category.ROLE, Action.EDIT_ROLES, [{
