4ea9ccd1d6921a9abd785e5d666adca1a336220a
kumo <h1710169@nushigh.edu.sg>
2021-11-11 23:29:12 +0800

    fix for missing images

--- backend/src/routes/collection.ts
+++ backend/src/routes/collection.ts
@@ -12,4 +12,6 @@ async function imageHandler(req: express.Request, res: express.Response) {
 
 collection.get("/:cid/images/:file", checkUser, imageHandler);
 collection.get('/:cid/img/:file', checkUser, imageHandler);
+collection.get("/:cid/:nid/images/:file", checkUser, imageHandler);
+
 export default collection;
