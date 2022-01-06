8291288acd2e5d1d374db114fc8a8ce6cb244cc8
kumo <h1710169@nushigh.edu.sg>
2022-01-06 14:23:42 +0800

    pruning

--- backend/src/routes/api/admin.ts
+++ backend/src/routes/api/admin.ts
@@ -2,9 +2,19 @@ import {Router} from "express";
 import {checkAdmin, checkUser} from "../../utils";
 import {db} from "../../app";
 import {teachers} from "../../config";
+import {COLLECTION_IMAGE_STORE, COLLECTION_NOTES_STORE, USERS_STORE} from "../../storage";
+import {error} from "../../response";
 
 const adminRoute = Router();
 adminRoute.get("/", checkUser, checkAdmin, (req, res) => res.json(teachers));
+adminRoute.get("/mapping", checkUser, checkAdmin, (req, res) => res.json({
+    images: COLLECTION_IMAGE_STORE._index,
+    notes: COLLECTION_NOTES_STORE._index,
+    users: USERS_STORE._index
+}))
+adminRoute.post("/prune", checkUser, checkAdmin, (req, res) => {
+    Promise.all([COLLECTION_IMAGE_STORE.prune(), COLLECTION_NOTES_STORE.prune(), USERS_STORE.prune()]).then(r => res.json(r)).catch(e => res.json(error(e)));
+})
 adminRoute.post("/directory", checkUser, checkAdmin, (req, res) => {
     let flag = false;
     const teachersRef = db.collection('config').doc('teachers');
